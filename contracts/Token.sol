3b44a7694f096543f5728fda64a10fffc4ac97e1
Pancho Labrador <plabradorjr@gmail.com>
2022-07-29 16:17:34 -0500

    added supply to deployer

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,6 +9,9 @@ contract Token {
     uint256 public decimals = 18;
     uint256 public totalSupply;
 
+    // Track balances
+    mapping(address => uint256) public balanceOf;
+
     constructor(
         string memory _name,
         string memory _symbol,
@@ -17,5 +20,6 @@ contract Token {
         name = _name;
         symbol = _symbol;
         totalSupply = _totalSupply * (10**decimals);
+        balanceOf[msg.sender] = totalSupply;
     }
 }
