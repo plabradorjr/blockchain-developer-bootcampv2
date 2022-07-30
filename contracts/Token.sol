cafb1d626d3175203b8bc7e6415f5bc4ce7a7747
Pancho Labrador <plabradorjr@gmail.com>
2022-07-29 21:19:49 -0500

    token transfers

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,8 +9,9 @@ contract Token {
     uint256 public decimals = 18;
     uint256 public totalSupply;
 
-    // Track balances
     mapping(address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
     constructor(
         string memory _name,
@@ -21,5 +22,19 @@ contract Token {
         symbol = _symbol;
         totalSupply = _totalSupply * (10**decimals);
         balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(balanceOf[msg.sender] >= _value);
+        require(_to != address(0));
+
+        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+        balanceOf[_to] = balanceOf[_to] + _value;
+
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 }
