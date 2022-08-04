56fa73c9d371aed2427bff7b4162b19e8c20ebca
Pancho Labrador <plabradorjr@gmail.com>
2022-08-03 19:56:44 -0500

    seeded exchange

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,11 +1,33 @@
 async function main() {
+  console.log(`Preparing deployment...\n`);
+
   // Fetch contract to deploy
   const Token = await ethers.getContractFactory("Token");
+  const Exchange = await ethers.getContractFactory("Exchange");
 
-  // Deploy contract
-  const token = await Token.deploy();
-  await token.deployed();
-  console.log(`Token Deployed to: ${token.address}`);
+  // Fetch accounts
+  const accounts = await ethers.getSigners();
+
+  console.log(
+    `Accounts fetched:\n${accounts[0].address}\n${accounts[1].address}\n`
+  );
+
+  // Deploy contracts
+  const pengu = await Token.deploy("Pengu Token", "PENGU", "1000000");
+  await pengu.deployed();
+  console.log(`PENGU Deployed to: ${pengu.address}`);
+
+  const mETH = await Token.deploy("mETH", "mETH", "1000000");
+  await mETH.deployed();
+  console.log(`mETH Deployed to: ${mETH.address}`);
+
+  const mDAI = await Token.deploy("mDAI", "mDAI", "1000000");
+  await mDAI.deployed();
+  console.log(`mDAI Deployed to: ${mDAI.address}`);
+
+  const exchange = await Exchange.deploy(accounts[1].address, 10);
+  await exchange.deployed();
+  console.log(`Exchange Deployed to: ${exchange.address}`);
 }
 
 main()
