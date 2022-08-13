c86ac1e04d2caca5548cec3d380999825de840e2
Pancho Labrador <plabradorjr@gmail.com>
2022-08-13 02:57:59 -0500

    trades

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,7 @@ import Markets from "./Markets";
 import Balance from "./Balance";
 import Order from "./Order";
 import PriceChart from "./PriceChart";
+import Trades from "./Trades";
 import Orderbook from "./Orderbook";
 
 function App() {
@@ -80,7 +81,7 @@ function App() {
 
           {/* Transactions */}
 
-          {/* Trades */}
+          <Trades />
 
           <Orderbook />
         </section>
